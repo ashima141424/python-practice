d8fbcb2bd9ae20d876255a44bfd625716178b092
Aashima <ashua94m@gmail.com>
2025-06-17 11:17:50 +0530

    submitting my day1 assignment

---
+++ day1/c2.py
@@ -0,0 +1,8 @@
+print(" Welcome to my to do list")
+prompt = input("Enter the to-do list\n ")
+prompt1 = input("Enter the to-do list \n")
+prompt2 = input("Enter the to-do list \n")
+prompt3 = input("Enter the to-do list \n")
+prompt4 = input("Enter the to-do list \n")
+list=[prompt,prompt1,prompt2,prompt3,prompt4]
+print(list)
